73d3b5cfc586ffafd216dcefe341e00a9227ccff
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-09-01 21:36:20 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
+import NewProduct from './pages/newProduct/NewProduct';
 
 function App() {
   return (
@@ -24,7 +25,7 @@ function App() {
           <Route path='/newUser' element={<NewUser/>}/>
           <Route path='/products' element={<ProductList/>}/> 
           <Route path='/products/:productId' element={<Product/>}/> 
-          <Route path='/newproduct' element={<NewUser/>}/>    
+          <Route path='/newproduct' element={<NewProduct/>}/>    
         </Routes>     
       </div>        
     </Router>
