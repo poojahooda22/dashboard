366eb1cf16e5cabdfab953517ae9f4747bbc7e85
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-09-02 22:14:18 +0530

    four

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,47 +2,59 @@ import React from 'react'
 import * as FaIcons from 'react-icons/fa';
 import './Sidebar.css';
 
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className='sidebar'>
         <div className='sidebarWrapper'>
             <div className='sidebarMenu'>
-                
-                <ul className='sidebarList'>
-                    <li className='sidebarListItem '>
-                        <FaIcons.FaHome className='sidebarIcon '/>
-                        Home
-                    </li>
+                <ul className='sidebarList'> 
+                    <Link to="/" className='link'>
+                        <li className='sidebarListItem '>
+                            <FaIcons.FaHome className='sidebarIcon '/>
+                            Home
+                        </li>
+                    </Link>
                     <li className='sidebarListItem'>
-                        <FaIcons.FaMailchimp className='sidebarIcon'/>
-                        Mails
+                        <FaIcons.FaChartBar className='sidebarIcon'/>
+                        Analytics
                     </li>
-                    <li className='sidebarListItem'>
-                        <FaIcons.FaList className='sidebarIcon'/>
-                        Projects
-                    </li>
+                    
+                    <Link to="/products" className='link' >
+                        <li className='sidebarListItem'>
+                            <FaIcons.FaList className='sidebarIcon'/>
+                            Products
+                        </li>
+                    </Link>
+                    <Link to="/users" className='link'>
+                        <li className="sidebarListItem">
+                            <FaIcons.FaUser className='sidebarIcon'/>
+                            Users
+                        </li>
+                    </Link>
                     
                     <li className='sidebarListItem'>
                         <FaIcons.FaChartLine className='sidebarIcon'/>
                         Reports
                     </li>
                     <li className='sidebarListItem'>
-                        <FaIcons.FaChartBar className='sidebarIcon'/>
-                        Analytics
+                        <FaIcons.FaMailchimp className='sidebarIcon'/>
+                        Mails
                     </li>
+                    
                     <li className='sidebarListItem'>
                         <FaIcons.FaFlag className='sidebarIcon'/>
                         Report
                     </li>
-                    <li className='sidebarListItem'>
+                    
+                    {/* <li className='sidebarListItem'>
                         <FaIcons.FaUser className='sidebarIcon'/>
                         User
-                    </li>
+                    </li> */}
                 </ul>
-            </div>
+            </div>    
         </div>
-     
     </div>
   )
 }
