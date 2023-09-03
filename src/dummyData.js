a6c70b5ce714e617ab25d78b30903280d3e7912f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-09-03 22:43:41 +0530

    dummy data

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,3 +1,19 @@
+import User1 from './assets/User1.png';
+import User2 from './assets/User2.png';
+import User3 from './assets/User3.png';
+import User4 from './assets/User4.png';
+import User5 from './assets/User5.png';
+import User6 from './assets/User6.png';
+import User7 from './assets/User7.png';
+
+import Product1 from './assets/Product1.png';
+import Product2 from './assets/Product2.png';
+import Product3 from './assets/Product3.png';
+import Product4 from './assets/Product4.png';
+import Product5 from './assets/Product5.png';
+import Product6 from './assets/Product6.png';
+
+
 export const userData = [
     {
       name: 'Jan',
@@ -119,3 +135,130 @@ export const data3 = [
     "color": 'red'
   },
 ]; 
+
+
+export const userRows = [
+  { id: 1, company: 'Catalog',
+  subHeading: "catalogapp.io",
+  avatar: <img className='userListImg'  src={User1} alt="" />,
+   status: "active",
+  assessed: "22 Jan 2021", 
+   about: "Content curatine app." },
+   { id: 2, company: 'Circooles',
+   subHeading: "getcircooles.com",
+  avatar: <img className='userListImg'  src={User2} alt="" />,
+   status: "active",
+   assessed: "20 Jan 2022",  
+   about: "Content curatine app." },
+   { id: 3, company: 'Command',
+   subHeading: "smdr.ai",
+  avatar: <img className='userListImg'  src={User3} alt="" />, 
+   status: "inactive",
+   assessed: "24Jan 2022",  
+   about: "Content curatine app." },
+   { id: 4, company: 'Hourglass',
+   subHeading: "hourglass.app",
+  avatar: <img className='userListImg'  src={User4} alt="" />,
+   status: "active",
+   assessed: "26 Jan 2022",  
+   about: "Content curatine app." },
+   { id: 5, company: 'Layers',
+   subHeading: "getlayers.io",
+  avatar: <img className='userListImg'  src={User5} alt="" />,
+   status: "active",
+   assessed: "18 Jan 2022",   
+   about: "Content curatine app." },
+   { id: 6, company: 'Quotient',
+   subHeading: "quotient.co",
+  avatar: <img className='userListImg'  src={User6} alt="" />,
+   status: "active", 
+   assessed: "28 Jan 2022", 
+   about: "Content curatine app." },
+   { id: 7, company: 'Sisyphus',
+   subHeading: "sisyphus.com",
+  avatar: <img className='userListImg'  src={User7} alt="" />,
+   status: "inactive",
+   assessed: "22 Jan 2022",  
+   about: "Content curatine app." },
+ 
+];
+
+export const productRows = [
+  { id: 1, product: 'Spotify',
+  subHeading: "catalogapp.io",
+  avatar: <img className='productListImg'  src={Product1} alt="" />,
+   status: "active",
+  price: "$230.00", 
+   stock: 23 },
+
+   { id: 2, product: 'Figma',
+   subHeading: "getcircooles.com",
+  avatar: <img className='productListImg'  src={Product2} alt="" />,
+   status: "active",
+   price: "$530.00", 
+   stock: 243 },
+
+   { id: 3, product: 'Stripe',
+   subHeading: "smdr.ai",
+  avatar: <img className='productListImg'  src={Product3} alt="" />, 
+   status: "inactive",
+   price: "$130.00", 
+   stock: 230 },
+
+   { id: 4, product: 'A Coffee',
+   subHeading: "hourglass.app",
+  avatar: <img className='productListImg'  src={Product4} alt="" />,
+   status: "active",
+   price: "$230.00", 
+   stock: 231 },
+
+   { id: 5, product: 'TB Bakery',
+   subHeading: "getlayers.io",
+  avatar: <img className='productListImg'  src={Product5} alt="" />,
+   status: "active",
+   price: "$230.00", 
+   stock: 273 },
+
+   { id: 6, product: 'Fresh F&V',
+   subHeading: "quotient.co",
+  avatar: <img className='productListImg'  src={Product6} alt="" />,
+   status: "active", 
+   price: "$730.00", 
+   stock: 213 },
+
+   { id: 7, product: 'Stripe',
+   subHeading: "sisyphus.com",
+  avatar: <img className='productListImg'  src={Product3} alt="" />,
+   status: "inactive",
+   price: "$430.00", 
+   stock: 283 },
+ 
+];
+
+export const productData = [
+  {
+    name: 'Jan',
+    "Sales": 4000, 
+  },
+  {
+    name: 'Feb',
+    "Sales": 5000,
+  },
+  {
+    name: 'Mar',
+    "Sales": 2000,
+  },
+  {
+    name: 'Apr',
+    "Sales": 3080,
+  },
+  {
+    name: 'May',
+    "Sales": 5890,
+  },
+  {
+    name: 'Jun',
+    "Sales": 2390,
+  },
+  
+];
