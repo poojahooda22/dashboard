83169de0ae8b150fa4a1ec7dcdd50a261a45b4ec
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-09-03 22:43:57 +0530

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
 import Chart from '../../components/chart/Chart'
 import { userData } from '../../dummyData'
+import WidgetLg from '../../components/widgetLg/WidgetLg'
+import WidgetSm from '../../components/widgetSm/WidgetSm'
 
 
 function Home() {
@@ -10,6 +12,11 @@ function Home() {
     <div className='home'>
       <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <div className='homeWidget'>
+        <WidgetSm />
+        <WidgetLg />
+        
+      </div>
     </div>
   );
 }
